b37bd197f7fba12c5868a440dfbeaf050e31258d
sushant <sushantsxzz5@gmail.com>
2025-11-18 00:36:18 +0530

    single audio stream hack to bypass ios autoplay restictions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState, useRef } from "react";
 import "./App.css";
 
+const sound = new URL("./assets/sounds/full_single_stream.mp3", import.meta.url)
+const audio = new Audio(sound.href);
+function hackAutoplayRestrictions() {
+  audio.play();
+  audio.pause();
+}
+
 function App() {
     return <>
+      <button onClick={hackAutoplayRestrictions}>Hack autoplay restrictions</button>
       <h2>Legendary meditation timer</h2>
       <h6>Because ek interval bell ke liye paisa maang rhe hain app waale</h6>
       <Timer />
@@ -16,7 +24,7 @@ function Timer() {
     const [displayBellInterval, setDisplayBellInteval] = useState(5);
     const intervalId = useRef(-1);
     const [playing, setPlaying] = useState(false);
-    const bellInterval = useRef(300);
+    const bellInterval = useRef(10);
 
     function prepareDisplayTime(rem) {
       // debugger;
@@ -90,13 +98,31 @@ function Timer() {
 }
 
 function playExitSounds(){
-  const sound = new URL("./assets/sounds/start_stop_bell.mp3", import.meta.url)
-  new Audio(sound.href).play()
+  audio.currentTime = 16.0;
+  setTimeout(()=>{
+    audio.pause();
+    console.log("pausing exit sound");
+  },2500)
+  audio.play();
+  console.log("playing exit sound");
 }
 function playChime(){
-  const sound = new URL("./assets/sounds/interval_bell.mp3", import.meta.url)
-  new Audio(sound.href).play()
+  audio.currentTime = 0.5;
+  setTimeout(()=>{
+    audio.pause();
+    console.log("pausing chime sound");
+  },5000)
+  audio.play();
+  console.log("playing chime sound");
 }
 
 
 export default App;
+
+
+
+
+// 00:00 pe interval bell ka audio start hota hai and goes on to upto 00:10. Useful is only the first five seconds.
+// 00:11 se 00:15 tak silent clip hai.
+// 00:16 se 00:19 tak start/stop bell ki clip hai. 
+// 00:20 se 00:25 tak again ek silent clip hai.
